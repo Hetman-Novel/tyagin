4487d7243daa7acce396dc5591e75c7811f1c784
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-07-06 13:16:16 +0300

    Add files via upload

--- files/swiper/js/init.js
+++ files/swiper/js/init.js
@@ -72,6 +72,35 @@ document.addEventListener('DOMContentLoaded', function () {
          museumHallsWrapSlider.classList.add('more-than-six');
       }
    }
+
+   const sliderBlocks = document.querySelectorAll('.about-tyagin__sliderBlockImage');
+   if (sliderBlocks) {
+      function handleEvent(event) {
+         document.querySelectorAll('.show-pagination').forEach(element => {
+            element.classList.remove('show-pagination');
+         });
+         let parent = event.currentTarget;
+         for (let i = 0; i < 4; i++) {
+            parent = parent.parentElement;
+            if (!parent) return;
+         }
+         parent.classList.add('show-pagination');
+      }
+      sliderBlocks.forEach(block => {
+         block.addEventListener('mouseenter', handleEvent);
+         block.addEventListener('touchstart', handleEvent);
+      });
+      sliderBlocks.forEach(block => {
+         block.addEventListener('mouseleave', function () {
+            let parent = this;
+            for (let i = 0; i < 4; i++) {
+               parent = parent.parentElement;
+               if (!parent) return;
+            }
+            parent.classList.remove('show-pagination');
+         });
+      });
+   }
 });
 
 const museumHallsBlock = document.querySelector('.museum-halls__block')
@@ -121,6 +150,77 @@ if (museumHallsBlock) {
       },
       watchSlidesProgress: true,
       watchSlidesVisibility: true,
+      simulateTouch: false,
+      touchRation: 0,
+      touchAngle: 0,
+      watchOverflow: false,
+   });
+}
+
+
+const aboutTyaginSliderOne = document.getElementById('about-tyagin-slider-one')
+if (aboutTyaginSliderOne) {
+   new Swiper(aboutTyaginSliderOne, {
+      pagination: {
+         el: "#about-tyagin-slider-one-pagination",
+         type: "fraction",
+      },
+      slidesPerView: 1,
+      loop: true,
+      freeMode: true,
+      autoplay: {
+         deley: 100,
+         stopOnLastSlide: false,
+         disableOnInteraction: false,
+      },
+      speed: 1200,
+      effect: 'fade',
+      simulateTouch: false,
+      touchRation: 0,
+      touchAngle: 0,
+      watchOverflow: false,
+   });
+}
+const aboutTyaginSliderTwo = document.getElementById('about-tyagin-slider-two')
+if (aboutTyaginSliderTwo) {
+   new Swiper(aboutTyaginSliderTwo, {
+      pagination: {
+         el: "#about-tyagin-slider-two-pagination",
+         type: "fraction",
+      },
+      slidesPerView: 1,
+      loop: true,
+      freeMode: true,
+      autoplay: {
+         deley: 300,
+         stopOnLastSlide: false,
+         disableOnInteraction: false,
+      },
+      speed: 1200,
+      effect: 'fade',
+      simulateTouch: false,
+      touchRation: 0,
+      touchAngle: 0,
+      watchOverflow: false,
+   });
+}
+const aboutTyaginSliderThree = document.getElementById('about-tyagin-slider-three')
+if (aboutTyaginSliderThree) {
+   new Swiper(aboutTyaginSliderThree, {
+      pagination: {
+         el: "#about-tyagin-slider-three-pagination",
+         type: "fraction",
+      },
+      slidesPerView: 1,
+      loop: true,
+      freeMode: true,
+      autoplay: {
+         deley: 600,
+         stopOnLastSlide: false,
+         disableOnInteraction: false,
+      },
+      speed: 1200,
+      effect: 'fade',
       simulateTouch: false,
       touchRation: 0,
       touchAngle: 0,
